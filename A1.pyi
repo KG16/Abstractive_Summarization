c47a7ec021cc74d06f12b908bc346b2d15dd243a
kg16 <kritigoyal16@gmail.com>
2018-02-28 11:45:12 +0530

    1st section of code of 3 sections
    Signed-off-by: kg16 <kritigoyal16@gmail.com>

--- A1.pyi
+++ A1.pyi
@@ -1,28 +1,22 @@
-import networkx as nx
-
-"""
-G=nx.Graph()
-# Add nodes and edges
-G.add_edge("Node1", "Node2")
-nx.draw(G, with_labels = True)
-plt.savefig('labels.png')
-"""
-
+import nltk
 
 class Opinosis_Graph():
-    def __init__(self, label, SID, PID):
+    def __init__(self, label, pos, SID, PID):
         label = label
+        pos = pos
         sid = []
         sid.append(SID)
         pid = []  #  .append(PID)
         pid.append(PID)
 
+    '''
     def create_graph:
         opinosisG = nx.Graph()
 
     def insert_node(self):
 
     def update_node(self):
+    '''
 
 
 def read_input_datafiles():
@@ -40,8 +34,12 @@ def opinosis_graph(lines_list):
     lines_list = read_input_datafiles()
     no_sentences = lines_list.__len__()
     for i in range(no_sentences):
-        word_list = lines_list[i].split()  # for  the current sentence only
-        sentence_size = word_list.__len__()
+        #word_list = lines_list[i].split()  # for  the current sentence only
+        text = nltk.word_tokenize(lines_list[i])
+        text_pos_tagged = nltk.pos_tag(text)
+        sentence_size = text_pos_tagged.__len__()
+        print(text_pos_tagged)
+        '''
         for j in range(sentence_size):
             LABEL = word_list[j]
             PID = j
@@ -52,3 +50,12 @@ def opinosis_graph(lines_list):
                 new_node.insert_node()  # do I need to pass parameters
             else:
                 new_node.update_node()
+                
+                """
+                G=nx.Graph()
+                # Add nodes and edges
+                G.add_edge("Node1", "Node2")
+                nx.draw(G, with_labels = True)
+                plt.savefig('labels.png')
+                """
+        '''
