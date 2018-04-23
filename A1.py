4af23ab659bcaf236b9c491eece1e1d9e2325e26
kg16 <kritigoyal16@gmail.com>
2018-04-23 14:48:17 +0530

    check1

--- A1.py
+++ A1.py
@@ -1,17 +1,20 @@
-# # import grammar_check
-# import fuzzywuzzy as fuzz
-#
-# # # tool = grammar_check.LanguageTool('en-GB')
-# # text = 'This are bad.'
-# # matches = tool.check(text)
-# # len(matches)
-# # grammar_check.correct(text, matches)
-#
-#
-# fuzz.token_sort_ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
-import language_check
+# import grammar_check
+import fuzzywuzzy as fuzz
 
-tool = language_check.LanguageTool('en-US')
-text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
-matches = tool.check(text)
-print(language_check.correct(text, matches))
+# # tool = grammar_check.LanguageTool('en-GB')
+# text = 'This are bad.'
+# matches = tool.check(text)
+# len(matches)
+# grammar_check.correct(text, matches)
+
+
+fuzz.token_sort_ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
+# import language_check
+#
+# tool = language_check.LanguageTool('en-US')
+# text = u'Yay yay YAY Brie Larson! Room was so SO GREAT!!!'
+# text=text.lower()
+# matches = tool.check(text)
+# print(text)
+# print(language_check.correct(text, matches))
+# print("HI")
--- All.py
+++ All.py
@@ -11,61 +11,65 @@ import GlobVars
 # import nltk
 graph_helper = dict()
 G = nx.DiGraph()
-candidates = []  # check if needed
-final_summary_sentences = []
 
 
-def opinosis_graph(lines_list):
+def create_graph(lines_list):
     no_sentences = lines_list.__len__()
     for i in range(no_sentences):
-        # text = nltk.word_tokenize("hi, I'm a person called Kriti.")
+        # text = nltk.word_tokenize("Wow, what a beautiful day.")
         # print(nltk.pos_tag(text))
         # word_list = lines_list[i].split()  # for  the current sentence only
-        word_list = re.findall(r"[\w']+|[.,!?;]", lines_list[i])
+        word_list = re.findall(r"[\w']+|[.,!?;]", lines_list[i])  # Separates punctuations from words
         sentence_size = word_list.__len__()
         for j in range(sentence_size):
             label = word_list[j].lower()
             pid = j  # zero based indexing
             sid = i
-            print(label, sid, pid)
+            # print(label, sid, pid)
             if label in graph_helper.keys():
                 graph_helper[label].append((sid, pid))
             else:
                 graph_helper[label] = [(sid, pid)]
                 G.add_node(label)
             if j > 0:  # not first word of sentence  i.e. pid>0
-                G.add_edge(word_list[j - 1], label)  # directed edge
+                if word_list[j - 1] != label:  # check if needed- Removes self loop
+                    G.add_edge(word_list[j - 1], label)  # directed edge
     nx.draw(G, with_labels=True)
-    print("no of selfloops  " + str(nx.number_of_selfloops(G)))
+    # print("no of selfloops  " + str(nx.number_of_selfloops(G))) # Output=0
     plt.savefig('labels.png')
 
 
 def vsn(node_v):
     avg = 0
     list_of_values = graph_helper[node_v]
-    list_of_values = sorted(list_of_values, key=itemgetter(1))
+    list_of_values = sorted(list_of_values, key=itemgetter(1))  # needed? Sorting on PID?
     for i in range(len(list_of_values)):
         avg += list_of_values[i][1]
     avg /= len(list_of_values)
-    print("avg vsn= " + str(avg) + "  len(list_of_values)= " + str(len(list_of_values)) + "  " + node_v)
-    if avg <= 2:
+    # print("avg vsn= " + str(avg) + "  len(list_of_values)= " + str(len(list_of_values)) + "  " + node_v) #Reanalyze
+    if avg <= 4:  #change parameter and compare results. Suggested=2.
         return 1
     return 0
 
 
 def pri_calc(curr_word, prev_word):
+    count = 0
+    for i in range(len(graph_helper[curr_word])):
+        for j in range(len(graph_helper[prev_word])):
+            if graph_helper[curr_word][i][0] != graph_helper[prev_word][j][0]:  # check
+                if abs(graph_helper[curr_word][i][1] - graph_helper[prev_word][j][1]) <= GlobVars.REDUNDANCY_PARA:
+                    count +=1
     return 0
 
 
 def ven(label):
-    if label in [".", ",", "but", "and", "yet", "or", "so"]:
+    if label in [".", ",", "but", "and", "yet", "or", "so", "!", "?"]:
         return 1
     return 0
 
 
 def check_valid_sentence(sentence):
-
-    return 0
+    return 1
 
 
 def path_score(redundancy, path_len):
@@ -73,21 +77,36 @@ def path_score(redundancy, path_len):
     return redundancy / path_len
 
 
-def traverse(c_list, node_v, score, pri_overlap, sentence, path_len):
+# def dfs_recursive(graph, vertex, path=[]):
+#     path += [vertex]
+#
+#     for neighbor in graph[vertex]:
+#         if neighbor not in path:
+#             path = dfs_recursive(graph, neighbor, path)
+#
+#     return path
+
+
+def traverse(c_list, node_v, score, pri_overlap, sentence, path_len, path):
     redundancy = len(graph_helper[node_v])  #wrong. only for first
     if redundancy >= GlobVars.REDUNDANCY_PARA:
         if ven(node_v):
-            if check_valid_sentence(sentence) ==1:
+            if check_valid_sentence(sentence) == 1:
                 final_score = score / path_len
                 c_list.append((sentence, final_score))  # check appending tuple in list
+                #check for resetting sentence and other variables
+
     for vn in G.neighbors(node_v):  # check if directed children only
-        pri_new = pri_overlap + pri_calc(vn, node_v)
-        # figure out PRI
-        new_sentence = sentence + " " + vn
-        new_path_len = path_len + 1
-        new_score = score + path_score(redundancy, new_path_len)
-        # add if collapsible
-        traverse(c_list, vn, new_score, pri_new, new_sentence, new_path_len)
+        if vn not in path:
+            pri_new = pri_overlap + pri_calc(vn, node_v)
+            # figure out PRI
+            path += vn
+            new_sentence = sentence + " " + vn
+            new_path_len = path_len + 1
+            new_score = score + path_score(redundancy, new_path_len)
+            # add if collapsible
+            traverse(c_list, vn, new_score, pri_new, new_sentence, new_path_len, path)
+            # what if no valid neighbor exists?
 
 
 def eliminate_duplicates(candidates):
@@ -103,26 +122,25 @@ def sort_by_path_score(candidates):
     return sorted(candidates, key=itemgetter(1))
 
 
-def next_best_sentence(candidates):
-    # delete that sentence from pool
-    return candidates
-
-
-def opinosis_summarization():
+def create_summary():
     # node_size=G.number_of_nodes()
     node_size = len(graph_helper)
     all_keys = graph_helper.keys()
+    candidates = []  # check if needed
+    final_summary_sentences = []
     for node_v in all_keys:
         if vsn(node_v) == 1:
             path_len = 1
             score = 0
             c_list = []
-            traverse(c_list, node_v, score, graph_helper[node_v], node_v, path_len)
+            path = []
+            traverse(c_list, node_v, score, graph_helper[node_v], node_v, path_len, path)
             candidates.extend(c_list)  # not append
     candidates1 = eliminate_duplicates(candidates)
     candidates2 = sort_by_path_score(candidates1)
     for i in range(GlobVars.SUMMARY_SIZE_PARA):
-        final_summary_sentences.extend(next_best_sentence(candidates2))  # shorted this part
+        final_summary_sentences.extend(candidates2[i][0])  # shorten this part
+    return final_summary_sentences
 
 
 def main():
@@ -132,8 +150,8 @@ def main():
         f = open(path + "\\" + file, 'r')
         lines_list = f.readlines()
         f.close()
-        opinosis_graph(lines_list)
-        opinosis_summarization()
+        create_graph(lines_list)
+        print(create_summary())  #print on file save it
         break
 
 
