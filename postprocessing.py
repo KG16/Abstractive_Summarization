ee920d973f8d8f3948ca956fdeea845034bb8e8f
kg16 <kritigoyal16@gmail.com>
2018-05-03 10:54:46 +0530

    almost done

--- postprocessing.py
+++ postprocessing.py
@@ -11,24 +11,32 @@ def main():
         f.close()
         # return
         f = open(path + "\\" + file, 'w')
-
         line = lines_list[0]
         string = ""
         print(line)
-        if 'a' <= lines_list[0] <= 'z':
+        flag = 0
+        if 'a' <= line[0] <= 'z':
             string += line[0].upper()
         for i in range(1, line.__len__()):
-            if (i + 2 < line.__len__() and line[i + 1] in ('.' or ',' or '!')) and line[i] == ' ':
-                if 'a' <= line[i + 2] <= 'z':
-                    string += '. '
-                    string += line[i + 2].upper()
-                    i += 1
-                    continue
+            if i + 1 < line.__len__() and (line[i + 1] in ('.' or ',' or '!')) and line[i] == ' ':
+                # if 'a' <= line[i + 2] <= 'z':
+                string += '. '
+                # string += line[i + 2].upper()
+                i += 1
+                if line[i + 1] == '.':
+                    flag = 1
+                continue
+            if flag:
+                if 'a' <= line[i] <= 'z':
+                    string += line[i].upper()
+                flag = 0
             # elif line[i - 1] != '.' or ' ':
             #     string += line[i]
             # elif line[i] == ' ' and line[i+1] != '.':
             #     string += line[i]
-            string += line[i]
+            else:
+                string += line[i]
+
         f.write(string)
         print(string)
         f.close()
